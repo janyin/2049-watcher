c9ac73cfadadf5089ffee2784759fac63fbc04d2
janyin <liujiangdu@gmail.com>
2021-02-14 15:20:16 +0800

    add new article

--- components/avatar.js
+++ components/avatar.js
@@ -1,6 +1,10 @@
 export default function Avatar({ name, picture }) {
   return (
-    <div className="flex items-center">
+    <div 
+      className="flex items-center"
+      style={{ cursor: 'pointer' }}
+      onClick={() => window.open('https://github.com/janyin')}
+    >
       <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
