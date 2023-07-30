5d72606beb1effa417d9a75ac41bfa412d9ab1ae
banana <liujiangdu@gmail.com>
2023-07-30 19:38:43 +0800

    fix: add lang

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         <body className="bg-white text-black dark:bg-black dark:text-white">
           <Main />
