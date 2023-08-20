d592edea496402b3f9b19b64b6da84df75b9c245
banana <liujiangdu@gmail.com>
2023-08-20 16:58:54 +0800

    feat: vercel-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/index.scss';
 import '../styles/prism.scss';
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
